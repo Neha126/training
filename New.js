d073ff6b7e3f4ca5c334534e22405f692bc3483f
Neha126 <79010335+Neha126@users.noreply.github.com>
2021-02-13 19:47:46 +0530

    Update New.js

--- New.js
+++ New.js
@@ -1,1 +1,25 @@
 console.log('hello Chiku :)');
+
+// 13-02-2021
+// 1: Write a JavaScript program that accept two integers and display the large:
+function large(a,b){
+  
+}
+large(19,12);
+
+// 2. Write a JavaScript program which iterates the integers from 1 to 100. 
+//.  But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+//.  For numbers which are multiples of both three and five print "FizzBuzz"
+
+function fizzbuzz(){
+
+}
+
+fizzbuzz();
+
+// 3: Write a JavaScript program to sum the multiples of 3 and 5 under 1000
+function multiplof(){
+
+}
+
+multiplof();
